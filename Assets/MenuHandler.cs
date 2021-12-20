1c26ece685c0d1118c01cfd41bd91e6a672e994f
xXLordXx-CoolDood <langlandsx@gmail.com>
2021-12-20 23:46:48 +0800

    MainMenuStart
    Started working on the main menu, all related assets will be in the main menu folder. Gonna finish it up tonight and tmrw

---
+++ Assets/MenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject camHolder;
+
+    public GameObject mainMenuHolder;
+    public GameObject handbookHolder;
+
+    public void showMainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        handbookHolder.SetActive(false);
+        Debug.Log("Main");
+    }
+
+    public void showHandbook()
+    {
+        mainMenuHolder.SetActive(false);
+        handbookHolder.SetActive(true);
+        Debug.Log("handbook");
+    }
+}
+
